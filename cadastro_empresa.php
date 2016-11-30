a2ebba0b3e8ab99f22679253c743ead2c6e86d41
occhapeco <occhapecosenai@gmail.com>
2016-11-30 17:59:19 -0200

    conectado parte I

--- cadastro_empresa.php
+++ cadastro_empresa.php
@@ -1,26 +1,56 @@
+<?php
+	require_once("conectar_service.php");
+
+	$alert = "";
+
+	if (isset($_POST["concluir"]))
+	{
+		$insert = -3;
+		if($_POST["senha"] == $_POST["senha1"])
+		{
+			$insert = $service->call('empresa.insert', array($_POST["nome_usuario"],$_POST["email"],$_POST["senha"],$_POST["razao_social"],$_POST["nome_fantasia"],$_POST["cnpj"],$_POST["celular"],$_POST["rua"],$_POST["num"],$_POST["complemento"],$_POST["cep"],$_POST["bairro"],$_POST["cidade_id"],$_POST["latitude"],$_POST["longitude"],$_POST["telefone"]));
+			if($insert == 0)
+				$alert = '<div class="alert alert-danger" style="margin-top: 10px;margin-bottom:-40px;"><span><b>CNPJ inválido!</b> Digite novamente.</span></div>';
+			elseif($insert == -1)
+				$alert = '<div class="alert alert-danger" style="margin-top: 10px;margin-bottom:-40px;"><span><b>Email ou CNPJ já cadastrados!</b> Reveja seus dados.</span></div>';
+			elseif($insert == -2)
+				$alert = '<div class="alert alert-danger" style="margin-top: 10px;margin-bottom:-40px;"><span><b>Endereço inválido!</b> Reveja seus dados.</span></div>';
+		}
+		else
+			$alert = '<div class="alert alert-danger" style="margin-top: 10px;margin-bottom:-40px;"><span><b>Senhas não correspondem!</b> Digite novamente.</span></div>';
+	}
+?>
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png" />
-	<link rel="icon" type="image/png" href="assets/img/favicon.png" />
-	<title>Paper Bootstrap Wizard by Creative Tim</title>
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <title>Paper Dashboard by Creative Tim</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
-	<!-- CSS Files -->
+
+    <!-- Bootstrap core CSS     -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-	<link href="assets/css/paper-bootstrap-wizard.css" rel="stylesheet" />
+    <link href="assets/css/paper-bootstrap-wizard.css" rel="stylesheet" />
 
-	<!-- CSS Just for demo purpose, don't include it in your project -->
-	<link href="assets/css/demo.css" rel="stylesheet" />
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet"/>
 
-	<!-- Fonts and Icons -->
-    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.css" rel="stylesheet">
-	<link href='https://fonts.googleapis.com/css?family=Muli:400,300' rel='stylesheet' type='text/css'>
-	<link href="assets/css/themify-icons.css" rel="stylesheet">
+    <!--  Paper Dashboard core CSS    -->
+    <link href="assets/css/paper-dashboard.css" rel="stylesheet"/>
+
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="assets/css/demo.css" rel="stylesheet" />
+
+    <!--  Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Muli:400,300' rel='stylesheet' type='text/css'>
+    <link href="assets/css/themify-icons.css" rel="stylesheet">
 	
 	<style type="text/css">
 		i{
@@ -31,18 +61,21 @@
 </head>
 
 <body>
+
 	<div class="image-container set-full-height" style="background-image: url('imgs/tex_mex.jpg')">
 	 	    <!--   Big container   -->
 	    <div class="container">
 	        <div class="row">
-		        <div class="col-sm-8 col-sm-offset-2">
 
+		        <div class="col-sm-8 col-sm-offset-2">
+					<?php echo $alert; ?>
 		            <!--      Wizard container        -->
 		            <div class="wizard-container" style="margin-top:50px">
 
 		                <div class="card wizard-card" data-color="orange" id="wizardProfile">
-		                    <form action="" method="">
-		                <!--        You can switch " data-color="orange" "  with one of the next bright colors: "blue", "green", "orange", "red", "azure"          -->
+		                    <form action="#" id="frm" method="post">
+		                    	<input type="hidden" name="latitude" id="latitude">
+		                    	<input type="hidden" name="longitude" id="longitude">
 
 		                    	<div class="wizard-header text-center">
 		                        	<h3 class="wizard-title">Cadastre-se</h3>
@@ -58,7 +91,7 @@
 												<div class="icon-circle">
 													<i class="ti-user"></i>
 												</div>
-												Institucional
+												Informações
 											</a>
 										</li>
 			                            <li>
@@ -66,7 +99,7 @@
 												<div class="icon-circle">
 													<i class="ti-settings"></i>
 												</div>
-												Usuário
+												Cidade
 											</a>
 										</li>
 			                            <li>
@@ -82,92 +115,113 @@
 		                        <div class="tab-content">
 		                            <div class="tab-pane" id="about">
 		                            	<div class="row">
-											<div class="col-sm-10 col-sm-offset-1">
+											<div class="col-sm-6">
 												<div class="form-group">
 													<label>Razão Social <small>(obrigatório)</small></label>
-													<input name="firstname" type="text" class="form-control" name="razao_social" id="razao_social" placeholder="Digite a razão social">
+													<input type="text" class="form-control" name="razao_social" id="razao_social" placeholder="Digite a razão social" required>
 												</div>
 												<div class="form-group">
 													<label>Nome Fantasia <small>(obrigatório)</small></label>
-													<input name="lastname" type="text" class="form-control" name="nome_fantasia" id="nome_fantasia" placeholder="Digite o nome fantasia">
+													<input type="text" class="form-control" name="nome_fantasia" id="nome_fantasia" placeholder="Digite o nome fantasia" required>
 												</div>
 												<div class="form-group">
 													<label>CNPJ <small>(obrigatório)</small></label>
-													<input name="number" type="number" class="form-control" name="cnpj" id="cnpj" placeholder="Informe seu CNPJ" required>
+													<input type="number" class="form-control" name="cnpj" id="cnpj" placeholder="Informe seu CNPJ" required>
 												</div>
 												<div class="form-group">
 													<label>Telefone <small>(obrigatório)</small></label>
-													<input name="number" type="number" class="form-control" name="celular" id="celular" placeholder="Informe um telefone para contato" required>
+													<input type="number" class="form-control" name="celular" id="celular" placeholder="Informe um telefone para contato" required>
+												</div>
+											</div>
+											<div class="col-sm-6">
+												<div class="form-group">
+													<label>Nome de usuário <small>(obrigatório)</small></label>
+													<input type="text" class="form-control" name="nome_usuario" id="nome_usuario" placeholder="Informe o nome de usuário" required>
+												</div>
+												<div class="form-group">
+													<label>Email <small>(obrigatório)</small></label>
+													<input type="email" class="form-control" name="email" id="email" placeholder="informe um email" required>
+												</div>
+												<div class="form-group">
+													<label>Senha <small>(obrigatório)</small></label>
+													<input type="password" class="form-control" name="senha" id="senha" placeholder="Digite sua senha" required>
+												</div>
+												<div class="form-group">
+													<label>Digite a senha novamente <small>(obrigatório)</small></label>
+													<input name="senha1" id="senha1" type="password" class="form-control" placeholder="Digite novamente sua senha" required>
 												</div>
 											</div>
 										</div>
 		                            </div>
 		                            <div class="tab-pane" id="account">
 		                                <div class="row">
-											<div class="col-sm-10 col-sm-offset-1">
-												<div class="form-group">
-													<label>Nome de usuário <small>(obrigatório)</small></label>
-													<input name="firstname" type="text" class="form-control" name="nome_usuario" id="nome_usuario" placeholder="Informe o nome que usar">
-												</div>
-												<div class="form-group">
-													<label>Email <small>(obrigatório)</small></label>
-													<input name="lastname" type="email" class="form-control" name="email" id="email" placeholder="informe um email">
-												</div>
-												<div class="form-group">
-													<label>Senha <small>(obrigatório)</small></label>
-													<input name="email" type="password" class="form-control" name="senha" id="senha" placeholder="Digite sua senha">
-												</div>
-												<div class="form-group">
-													<label>Digite a senha novamente <small>(obrigatório)</small></label>
-													<input name="senha1" id="senha1" type="password" class="form-control" placeholder="Digite novamente sua senha">
-												</div>
-											</div>
+										<?php
+											$json = $service->call("select_cidades",array());
+											$cidade = json_decode($json);
+											for($i=0;$i<count($cidade);$i++)
+											{
+												$first = "checked";
+												$class_first = 'class="choice active"';
+												if($i > 0)
+												{
+													$first = "";
+													$class_first = 'class="choice"';
+												}
+												$str = "<p>".$cidade[$i]->nome." - ".$cidade[$i]->uf."</p>";
+												$str2 = "onclick='trocar(`".$cidade[$i]->nome."`,`".$cidade[$i]->uf."`)'";
+										?>
+											<div class="col-sm-3" >
+                                                <div <?php echo $class_first; ?> data-toggle="wizard-radio" <?php echo $str2; ?>>
+                                                    <input type="radio" name="cidade_id" <?php echo 'value="'.$cidade[$i]->id.'" '.$first; ?>>
+                                                    <div class="card card-radioss card-hover-effect">
+                                                    <?php echo $str; ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+										<?php
+											}
+											echo '<input type="hidden" name="cidade" id="cidade" value="'.$cidade[0]->nome.'"><input type="hidden" name="uf" id="uf" value="'.$cidade[0]->uf.'">'
+										?>
 										</div>
 		                            </div>
 		                            <div class="tab-pane" id="address">
 		                                <div class="row">
 		                                    <div class="col-sm-12">
 		                                    </div>
-		                                    <div class="col-sm-6">
-		                                        <div class="form-group">
-		                                            <label>País</label>
-		                                            <input type="text" class="form-control" name="pais" id="pais" placeholder="Brasil">
-		                                        </div>
-		                                    </div>
-		                                    <div class="col-sm-6">
-		                                        <div class="form-group">
-		                                            <label>Estado</label>
-		                                            <input type="text" class="form-control" name="uf" id="uf" placeholder="São Paulo">
-		                                        </div>
-		                                    </div>
-											<div class="col-sm-6">
-		                                        <div class="form-group">
-		                                            <label>Cidade</label>
-		                                            <input type="text" class="form-control" name="cidade" id="cidade" placeholder="São Paulo">
-		                                        </div>
-		                                    </div>
 											<div class="col-sm-6">
 		                                        <div class="form-group">
 		                                            <label>Bairro</label>
-		                                            <input type="text" class="form-control" name="bairro" id="bairro" placeholder="Centro">
+		                                            <input type="text" class="form-control" name="bairro" id="bairro" placeholder="Centro" required>
 		                                        </div>
 		                                    </div>
-		                                    <div class="col-sm-12">
+		                                    <div class="col-sm-6">
 		                                    	<div class="form-group">
 		                                            <label>Rua</label>
-		                                            <input type="text" class="form-control" name="rua" id="rua" placeholder="Avenida Paulista">
+		                                            <input type="text" class="form-control" name="rua" id="rua" placeholder="Avenida Paulista" required>
+		                                        </div>
+		                                    </div>
+		                                    <div class="col-sm-6">
+		                                    	<div class="form-group">
+		                                            <label>CEP</label>
+		                                            <input type="number" class="form-control" name="cep" id="cep" placeholder="89567231" required>
 		                                        </div>
 		                                    </div>
 		                                    <div class="col-sm-6">
 		                                        <div class="form-group">
 		                                            <label>Número</label>
-		                                            <input type="number" class="form-control" name="num" id="num" placeholder="242">
+		                                            <input type="number" class="form-control" name="num" id="num" placeholder="242" required>
 		                                        </div>
 		                                    </div>
 		                                    <div class="col-sm-6">
 		                                    	<div class="form-group">
 		                                            <label>Complemento</label>
-		                                            <input type="text" class="form-control" name="rua" id="rua" placeholder="Próx. à escola">
+		                                            <input type="text" class="form-control" name="complemento" id="complemento" placeholder="Próx. à escola">
+		                                        </div>
+		                                    </div>
+		                                    <div class="col-sm-6">
+		                                    	<div class="form-group">
+		                                            <label>Telefone</label>
+		                                            <input type="number" class="form-control" name="telefone" id="telefone" placeholder="4998989898">
 		                                        </div>
 		                                    </div>
 										</div>
@@ -176,7 +230,7 @@
 		                        <div class="wizard-footer">
 		                            <div class="pull-right">
 		                                <input type='button' class='btn btn-next btn-fill btn-warning btn-wd' name='next' value='Próximo' />
-		                                <input type='button' class='btn btn-finish btn-fill btn-warning btn-wd' name='finish' value='Concluir' />
+		                                <input type='submit' class='btn btn-finish btn-fill btn-warning btn-wd' name='concluir' value='Concluir' />
 		                            </div>
 
 		                            <div class="pull-left">
@@ -196,15 +250,52 @@
 
 </body>
 
-	<!--   Core JS Files   -->
-	<script src="assets/js/jquery-2.2.4.min.js" type="text/javascript"></script>
-	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="assets/js/jquery.bootstrap.wizard.js" type="text/javascript"></script>
+	<!-- Paper Dashboard DEMO methods, don't include it in your project! -->
+    <script src="assets/js/demo.js"></script>
 
-	<!--  Plugin for the Wizard -->
-	<script src="assets/js/paper-bootstrap-wizard.js" type="text/javascript"></script>
+    <!--   Core JS Files   -->
+    <script src="assets/js/jquery-2.2.4.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/js/jquery.bootstrap.wizard.js" type="text/javascript"></script>
 
-	<!--  More information about jquery.validate here: http://jqueryvalidation.org/	 -->
-	<script src="assets/js/jquery.validate.min.js" type="text/javascript"></script>
+    <!--  Plugin for the Wizard -->
+    <script src="assets/js/paper-bootstrap-wizard.js" type="text/javascript"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="assets/js/bootstrap-notify.js"></script>
+
+    <!-- Paper Dashboard Core javascript and methods for Demo purpose -->
+    <script src="assets/js/paper-dashboard.js"></script>
+
+    <!--  More information about jquery.validate here: http://jqueryvalidation.org/  -->
+    <script src="assets/js/jquery.validate.min.js" type="text/javascript"></script>
+
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAmWPAIE9_AASg6Ijgoh0lVOZZ_VWvw6fg&libraries=places&callback=initAutocomplete" async defer></script>  
+
+	<script type="text/javascript">
+		var geocoder;
+
+        function codeAddress() {
+        var address = document.getElementById( 'cidade' ).value+', '+document.getElementById( 'uf' ).value+ ', '+ document.getElementById( 'rua' ).value+' '+ document.getElementById( 'num' ).value;
+          geocoder.geocode( { 'address' : address }, function( results, status ) {
+            if( status == google.maps.GeocoderStatus.OK ) {
+                document.getElementById( 'latitude' ).value = results[0].geometry.location.lat();
+                document.getElementById( 'longitude' ).value = results[0].geometry.location.lng();
+                document.getElementById('frm').submit();
+            } else {
+                alert( 'Não podemos encontrar sua localização corretamente, por favor, reveja os dados.');
+            }
+        } );
+      }
+
+        function trocar(cidade,uf)
+        {
+        	document.getElementById('cidade').value = cidade;
+        	document.getElementById('uf').value = uf;
+        }
+
+	}
+
+    </script>
 
 </html>
