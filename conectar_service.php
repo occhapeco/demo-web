3f845af090c4e6c36962f7b1292c27e2f51ad0d4
occhapeco <occhapecosenai@gmail.com>
2016-12-21 11:16:30 -0200

    imgs

--- conectar_service.php
+++ conectar_service.php
@@ -2,7 +2,7 @@
 	require_once('lib/nusoap.php');
 
 	ini_set("soap.wsdl_cache_enabled", "1");
-	$service = new nusoap_client('service/index.php?wsdl', true);
+	$service = new nusoap_client('noxgames.com.br/clube/service/', true);
 
 	error_reporting(E_ALL);
 	ini_set("display_errors",0);
