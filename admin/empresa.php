83a316b16e112a3b05c1314050bfa8676bfc6a63
Andrew Malta <occhapecosenai@gmail.com>
2017-01-11 15:40:16 -0200

    Correções

--- admin/empresa.php
+++ admin/empresa.php
@@ -19,13 +19,13 @@
 <html lang="pt">
 <head>
 	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <link rel="icon" type="image/png" sizes="96x76" href="imgs/logo/escudo_clube.png">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>Clube de Ofertas</title>
+    <title>Clube de Ofertas</title>
 
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
 
