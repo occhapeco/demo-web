06b0ed9d6cd08daa6510600c6bce40fe60da8790
occhapeco <occhapecosenai@gmail.com>
2017-02-01 16:36:40 -0200

    Sem logo na aba

--- admin/financeiro.php
+++ admin/financeiro.php
@@ -34,7 +34,6 @@
 <html lang="pt">
 <head>
 	<meta charset="utf-8" />
-    <link rel="icon" type="image/png" sizes="96x76" href="../imgs/logo/escudo_clube.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
     <title>Clube de Ofertas</title>
--- admin/index.php
+++ admin/index.php
@@ -31,8 +31,6 @@
 <html lang="pt">
 <head>
 	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" sizes="96x76" href="../imgs/logo/escudo_clube.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 	<title>Clube de Ofertas</title>
