6a8b69d52811bef36ab64c8f78590545733fb13f
occhapeco <occhapecosenai@gmail.com>
2017-02-18 15:25:48 -0200

    Detalhes da empresa no modal

--- admin/empresa.php
+++ admin/empresa.php
@@ -84,7 +84,9 @@
 										}
 								?>
 								<tr>
-									<td><?php echo $empresa[$i]->razao_social; ?></td>
+									<td>
+										<a data-toggle="modal" href="#myModal" onclick = "passar_dados('<?php echo $empresa[$i]->razao_social?>','<?php echo $empresa[$i]->nome_fantasia?>','<?php echo $empresa[$i]->cnpj?>','<?php echo $empresa[$i]->email?>','<?php echo $empresa[$i]->celular?>','<?php echo $empresa[$i]->descricao?>','<?php echo $enderecos?>');" style="color:#66615B"><?php echo $empresa[$i]->razao_social; ?></a>
+									</td>
 									<td><?php echo $empresa[$i]->cnpj; ?></td>
 									<td><?php echo $empresa[$i]->celular; ?></td>
 									<td><?php echo $enderecos; ?></td>
@@ -123,7 +125,38 @@
             </div>
 		</div>
     </div>
+</div>
 
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+	      <div class="modal-content">
+	        <div class="modal-header" style="background-color:#F78131;">
+	          <button type="button" class="close" data-dismiss="modal">&times;</button>
+	          <h4 class="modal-title">Detalhes da Empresa</h4>
+	        </div>
+	        <div class="modal-body">
+				<div class="row">
+					<div class="col-xs-12">
+						<div>
+							<label>Razão Social:</label><label style="color:#252422" id="razao_social"></label><br>
+							<label>Nome Fantasia:</label><label style="color:#252422" id="nome_fantasia"></label><br>
+							<label>CPF/Cnpj:</label><label style="color:#252422" id="cnpj"></label><br>
+							<label>Email:</label><label style="color:#252422" id="email"></label><br>
+							<label>Telefone do responsável:</label><label style="color:#252422" id="telefone"></label><br>
+							<label>Descrição:</label><label style="color:#252422" id="descricao"></label><br>
+							<label>Endereço:</label><label style="color:#252422" id="endereco"></label>
+						</div>
+					</div>
+				</div>
+			</div>
+	      </div>
+
+    </div>
+  </div>
+	
+  
     <!--   Core JS Files   -->
     <script src="../assets/js/jquery-1.10.2.js" type="text/javascript"></script>
 	<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
@@ -142,6 +175,20 @@
     <!-- Paper Dashboard Core javascript and methods for Demo purpose -->
 	<script src="../assets/js/paper-dashboard.js"></script>
 	
+	<script>
+	function passar_dados(razao_social,nome_fantasia,cnpj,email,telefone,descricao,endereco)
+	{
+		document.getElementById("razao_social").innerHTML = razao_social;
+		document.getElementById("nome_fantasia").innerHTML = nome_fantasia;
+		document.getElementById("cnpj").innerHTML = cnpj;
+		document.getElementById("email").innerHTML = email;
+		document.getElementById("telefone").innerHTML = telefone;
+		document.getElementById("descricao").innerHTML = descricao;
+		document.getElementById("endereco").innerHTML = endereco;
+	}
+	</script>
+	
 </body>
+
 
 </html>
