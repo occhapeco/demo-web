df781d3f1267581f45bb8be1eb5d4c97ef0685bc
Andrew Malta <andrewsaxx@gmail.com>
2017-06-03 15:40:36 -0300

    Inutilizando NuSOAP

--- conectar_service.php
+++ conectar_service.php
@@ -1,9 +1,4 @@
 <?php
-	require_once('lib/nusoap.php');
-
-	ini_set("soap.wsdl_cache_enabled", "1");
-	$service = new nusoap_client('http://clubedeofertas.net/service/index.php?wsdl', true);
-
 	error_reporting(E_ALL);
 	ini_set("display_errors",0);
 
